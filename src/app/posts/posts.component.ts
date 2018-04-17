eb55bcfc9e2dad13716b70d9569cc3543e1c4cd6
aleksandr <aleksandr.trokhimenko@devellar.com>
2018-04-17 12:49:40 +0300

    edit posts components

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,82 +1,79 @@
-import  { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { PostsService } from '../shared/posts.service';
+import {PostsService} from '../shared/posts.service';
 
 @Component({
-	selector: 'app-posts',
-	templateUrl: './posts.component.html',
-	styleUrls: ['./posts.component.css']
+    selector: 'app-posts',
+    templateUrl: './posts.component.html',
+    styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit{
+export class PostsComponent implements OnInit {
 
-	posts = [];
-	category = [];
-	enabled = false
+    posts = [];
+    category = [];
+    enabled = false;
 
-	constructor(private postsService: PostsService){
-	}
+    constructor(private postsService: PostsService) {
+    }
 
-	ngOnInit(){
+    ngOnInit() {
         this.getPosts();
-	}
+    }
 
-    getPosts(){
-		this.postsService.getPosts()
-    	.subscribe(posts => {
-            this.posts =  posts;
-            this.category = this.getCategories(posts)
+    getPosts() {
+        this.postsService.getPosts()
+            .subscribe(posts => {
+                this.posts = posts;
+                this.category = this.getCategories(posts);
+            });
+    }
+
+    getCategories(posts) {
+        let obj = {};
+        let arr = posts.filter((post) => {
+            if (obj[post.category]) return obj[post.category];
+            obj[post.category] = true;
         });
-	}
+        return arr;
+    }
 
-	getCategories(posts){
-		let obj = {}
-		let arr = posts.filter((post) => {
-			if(obj[post.category]) return obj[post.category]
-			obj[post.category] = true;
-		})
-		return arr
-	}
-
-    onChangeCheckbox(value){
+    onChangeCheckbox(value) {
 
         this.getPosts();
 
         let searcPostList = [];
 
-            this.posts.forEach((post) => {
-                if( post.category === value.text.toLowerCase() ){
-					if(value.categoryName === true){
-							post.isActive = true;
-					}else{
-							post.isActive = false;
-					}
-				}
-			})
-        this.enabled = false
+        this.posts.forEach((post) => {
+            if (post.category === value.text.toLowerCase()) {
+                if (value.categoryName === true) {
+                    post.isActive = true;
+                } else {
+                    post.isActive = false;
+                }
+            }
+        });
+        this.enabled = false;
         searcPostList = this.posts.filter((post) => {
-            if(post.isActive ){
-            	this.enabled = true
-                return post
+            if (post.isActive) {
+                this.enabled = true;
+                return post;
             }
         }
 
-		if(searcPostList.length === 0) return
-		this.posts = searcPostList;
-	}
+        if (searcPostList.length === 0) return;
+        this.posts = searcPostList;
+    }
 
-	onChange(value){
-		this.getPosts();
-		let searchUsersList = this.posts.filter((post) => {
-				if(this.enabled){
-					console.log('isAvtive')
-                    return post.title.indexOf(value.toLowerCase()) !== -1 && post.isActive === true;
-
-				}else{
-                    console.log('MOT isAvtive')
-                    return post.title.indexOf(value.toLowerCase()) !== -1
-				}
-		});
-		return this.posts = searchUsersList
-	}
+    onChange(value) {
+        this.getPosts();
+        let searchUsersList = this.posts.filter((post) => {
+            if (this.enabled) {
+                return post.title.indexOf(value.toLowerCase()) !== -1 && post.isActive === true;
+            } else {
+                return post.title.indexOf(value.toLowerCase()) !== -1;
+            }
+        });
+        return this.posts = searchUsersList;
+    }
 
 }
